f8552c16cf3ffa083473a9bce57bf861bc195e91
Tristan Zaleski <zaleski.tristan@gmail.com>
2021-09-02 19:39:38 -0500

    Step 2 Complete. Home page reflects collection of posts in correct format, stored within database.

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
 
 // must have space between app and other constants, as they will not register if written directly under the express invocation
 //############## MongoDB CONTS ##########################
-let posts = [];
 
 const postSchema = {
   title: {
@@ -43,15 +43,14 @@ app.use(express.static("public"));
 
 app.get("/", function(req,res){
   //console.log(posts);
-  res.render("home", {
-    homeContent: homeStartingContent,
-    newPostList: posts
+  Post.find({}, function(err, posts){
+   res.render("home", {
+     startingContent: homeStartingContent,
+     posts: posts
+     });
+   });
   });
-});
 
-app.get("/about", function(req,res){
-  res.render("about", {abtContent: aboutContent});
-});
 
 app.get("/posts/:postT", function(req, res){
   const title = _.lowerCase(req.params.postT);
@@ -71,6 +70,10 @@ app.get("/posts/:postT", function(req, res){
   });
   //res.render("post");
 })
+
+app.get("/about", function(req,res){
+  res.render("about", {abtContent: aboutContent});
+});
 
 app.get("/contact", function(req,res){
   res.render("contact", {contContent: contactContent});
